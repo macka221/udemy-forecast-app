f5465196e6b2f9fbf441e0a1529c9bf7516e38aa
Zach <zachare18@gmail.com>
2025-04-08 22:24:18 -0400

    Final commit before turning in for the day

--- react-native-meteo/App.js
+++ react-native-meteo/App.js
@@ -1,10 +1,51 @@
 import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
+import { useEffect, useState } from 'react';
 import { ImageBackground } from 'react-native';
+import { requestForegroundPermissionsAsync, getCurrentPositionAsync } from 'expo-location';
 import { appStyles } from './app.style';
-import { Home } from './pages/home/home'; 
+import { Home } from './pages/home/home';
+import { MeteoAPI } from './service/meteo';
 import ForecastBackground from './assets/background.png';
 
 export default function App() {
+  const [coordinates, setCoordinates] = useState();
+  const [weather, setWeather] = useState();
+
+  useEffect(() => {
+   getUserCoordinates();
+  }, []);
+
+  useEffect(() => {
+    if (coordinates) {
+      fetchWeatherByCoords(coordinates);
+    }
+  }, [coordinates])
+
+  async function fetchWeatherByCoords(coords) {
+    const weatherResponse = await MeteoAPI.fetchWeatherByCoords(coords);
+    setWeather(weatherResponse);
+  };
+ 
+  async function getUserCoordinates() {
+    const { status } = await requestForegroundPermissionsAsync();
+
+    if (status === 'granted') {
+      const location = await getCurrentPositionAsync();
+      setCoordinates({
+        lat: location.coords.latitude,
+        lng: location.coords.longitude,
+      });
+    } else {
+      setCoordinates({
+        lat: '48.85',
+        lng: '2.35',
+      });
+    }
+  }
+
+  console.log(coordinates);
+  console.log(weather);
+
   return (
     <ImageBackground 
       source={ ForecastBackground } 
